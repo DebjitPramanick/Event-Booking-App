4d3b4752270d96c1a9ce146aaeacb50c5e00a9d6
DebjitPramanick <dpramanick9990@gmail.com>
2021-07-02 13:06:52 +0530

    Frontend deployed v3

--- client/src/utils/AppContext.js
+++
@@ -1,8 +0,0 @@
-import {createContext} from 'react'
-
-export const AppContext = createContext({
-    token: null,
-    userId: null,
-    login: (userId, token, tokenExp) => {},
-    logout: () => {}
-})
